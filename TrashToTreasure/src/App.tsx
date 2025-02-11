1ade875e2cd67e8a444ef692cb668798b4990c0d
antiquark007 <141497727+antiquark007@users.noreply.github.com>
2025-02-11 14:58:47 +0530

    added files

--- TrashToTreasure/src/App.tsx
+++ TrashToTreasure/src/App.tsx
@@ -1,6 +1,27 @@
+import NavBar from "./components/NavBar"
+
 const App = () => {
+  const navItems = [
+    { title: 'Home', link: '/' },
+    { title: 'Products', link: '/products' },
+    { title: 'Categories', link: '/categories', 
+      dropdownItems: [
+        { title: 'Furniture', link: '/categories/furniture' },
+        { title: 'Electronics', link: '/categories/electronics' },
+        { title: 'Art', link: '/categories/art' }
+      ]
+    },
+    { title: 'About', link: '/about' },
+    { title: 'Contact', link: '/contact' }
+  ];
+
   return (
-    <div>App</div>
+    <>
+      <NavBar 
+        brandName="Trash To Treasure" 
+        items={navItems}
+      />
+    </>
   )
 }
 
