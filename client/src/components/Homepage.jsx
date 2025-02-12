bccef5decf4c4ea99f4038a23bb32820f26dd930
Adwait Aayush <adwaitaayush80@gmail.com>
2025-02-12 15:49:12 +0530

    initial commit

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -5,17 +5,7 @@ import Footer from './Footer';
 const Homepage = ({ email, fname }) => {
   return (
     <>
-    <div className="homepage-container">
-      <h1>Welcome, {fname}!</h1>
-      <p>Your email: {email}</p>
-      <p>
-        Welcome to our platform! We are thrilled to have you here. Explore our features and enjoy your stay.
-      </p>
-      <p>
-        If you have any questions or need assistance, feel free to reach out to our support team.
-      </p>
-    </div>
-    <Footer />
+     
     </>
 
   );
